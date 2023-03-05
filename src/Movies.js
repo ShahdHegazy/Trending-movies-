8d54f971d194f02859cbfb1571589c7825d2f74e
ShahdHegazy <127021016+ShahdHegazy@users.noreply.github.com>
2023-03-05 02:49:46 -0800

    Add files via upload

---
+++ src/Movies.js
@@ -0,0 +1,50 @@
+import { Component } from "react";
+import axios from "axios";
+export default class Movies extends Component{
+
+state={movies:[]};
+
+
+async componentDidMount(){
+
+let x=await axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=52bbcddeda849047525b51d6f8a12361');
+this.setState({movies:x.data.results})
+
+
+
+}
+
+
+
+
+
+render(){
+    return <div>
+
+<div class = "container" >
+<div className = "row" >
+<div className="col-md-3">
+<div className="brd w-50"></div>
+<h3>Trending <br />Movies<br />to watch now</h3>
+<p className="secondColor">most watched movies by days</p> 
+<div className="brd w-100"></div>
+</div>
+
+{this.state.movies.map((value,index)=>{
+
+return <div key={index} className="col-md-3">
+<img src={"https://image.tmdb.org/t/p/original"+value.poster_path} className="w-100" />
+<p>{value.title}{value.name}</p>
+</div>
+
+
+})}
+
+</div>
+</div>
+    </div>
+}
+
+
+
+}
---
+++ src/Right.js
@@ -0,0 +1,31 @@
+import {Component} from 'react';
+
+
+
+
+export default class Right extends Component{
+    render(){
+return <div>
+
+<div className = "right" >
+            <hr class = "l1" />
+            <h1 class = "title" > Trending </h1>
+             <h1 class = "title" > Movies </h1> 
+            <h1 class = "title" > To Watch Now </h1>
+             <p > most watched movies by days </p>
+
+        <hr class = "l2" />
+            </div>
+
+
+
+
+
+</div>
+
+
+
+
+
+    }
+}
